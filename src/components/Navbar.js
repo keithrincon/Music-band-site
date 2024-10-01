e58ec03985049b54c922f4a7e1f07699800dc04e
Keith Rincon <keithrt3008@gmail.com>
2024-10-01 14:44:57 -0700

    fixed nav scroll effect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,25 @@
-import React from 'react';
-// import logo from '../images/logo.svg';
+import React, { useEffect, useState } from 'react';
 import { socialLinks } from '../data';
 import PageLinks from './PageLinks';
 import SocialLink from './SocialLink';
 
 function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    setIsScrolled(offset > 50); // Change 50 to the desired scroll value
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <nav className='navbar'>
+    <nav className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
       <div className='nav-center'>
         <div className='nav-header'>
-          {/* <img src={logo} className='nav-logo' alt='backroads' /> */}
           <h2>Novedad</h2>
           <button type='button' className='nav-toggle' id='nav-toggle'>
             <i className='fas fa-bars'></i>
@@ -19,9 +29,9 @@ function Navbar() {
         <PageLinks parentClass='nav-links' itemClass='nav-link' />
 
         <ul className='nav-icons'>
-          {socialLinks.map((link) => {
-            return <SocialLink key={link.id} {...link} itemClass='nav-icon' />;
-          })}
+          {socialLinks.map((link) => (
+            <SocialLink key={link.id} {...link} itemClass='nav-icon' />
+          ))}
         </ul>
       </div>
     </nav>
