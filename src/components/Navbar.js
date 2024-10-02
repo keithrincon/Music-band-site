0511797ac32e5f1dd829b826cd7fd6da6fb3c5a5
Keith Rincon <keithrt3008@gmail.com>
2024-10-01 17:06:08 -0700

    nav bar hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,11 @@ import SocialLink from './SocialLink';
 
 function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [showLinks, setShowLinks] = useState(false); // State for toggling the social icons
 
   const handleScroll = () => {
     const offset = window.scrollY;
-    setIsScrolled(offset > 50); // Change 50 to the desired scroll value
+    setIsScrolled(offset > 50);
   };
 
   useEffect(() => {
@@ -16,19 +17,31 @@ function Navbar() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const toggleLinks = () => {
+    setShowLinks((prev) => !prev); // Toggle the visibility of social icons
+  };
+
   return (
     <nav className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
       <div className='nav-center'>
         <div className='nav-header'>
           <h2>Novedad</h2>
-          <button type='button' className='nav-toggle' id='nav-toggle'>
+          {/* Hamburger Icon */}
+          <button
+            type='button'
+            className='nav-toggle'
+            id='nav-toggle'
+            onClick={toggleLinks}
+          >
             <i className='fas fa-bars'></i>
           </button>
         </div>
 
+        {/* Page Links */}
         <PageLinks parentClass='nav-links' itemClass='nav-link' />
 
-        <ul className='nav-icons'>
+        {/* Show social icons only when showLinks is true */}
+        <ul className={`nav-icons ${showLinks ? 'show-icons' : ''}`}>
           {socialLinks.map((link) => (
             <SocialLink key={link.id} {...link} itemClass='nav-icon' />
           ))}
